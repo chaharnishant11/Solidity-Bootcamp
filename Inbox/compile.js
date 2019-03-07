9b660e2b7a1b4737f5f654f08fdfbe3ad8721f56
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-03-07 20:39:34 +0530

    adding files

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -6,3 +6,4 @@ const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
 
 module.exports = solc.compile(source,1).contracts[':Inbox'];
+//interface == javascript ABI and bytecode is the actual raw compiled contract 
--- Inbox/test/inbox.test.js
+++ Inbox/test/inbox.test.js
@@ -3,21 +3,29 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 //whenever we use constructors the first letter of the variable is capitalised
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
+let accounts;
+let inbox;
 
-class Car {
-  park(){
-    return 'stopped';
-  }
+beforeEach(async () => {
+  //Get a list of all accounts
+  //Promices example
+  // web3.eth.getAccounts()
+  //   .then(fetchAccounts => {
+  //     console.log(fetchAccounts);
+  //   });
+  //async await(much more cleaner)
+  accounts = await web3.eth.getAccounts();
+  //use one of those accounts to deploy the contracts
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000'});
 
-  drive(){
-    return 'vroom';
-  }
-}
+});
 
-describe('Car', () => {
-  it('can park',()=> {
-    const car = new Car();
-    assert.equal(car.park(),'stopped');
+describe('Inbox',() => {
+  it('deploy a contract',() => {
+    console.log(inbox);
   });
 });
