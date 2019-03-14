ee5164f928be08542ff1b0aa98ac14180660e9e7
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-03-14 22:52:15 +0530

    updating files

--- lottery/compile.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
-
-const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
-const source = fs.readFileSync(inboxPath,'utf8');
-
-module.exports = solc.compile(source,1).contracts[':Inbox'];
-//interface == javascript ABI and bytecode is the actual raw compiled contract 
