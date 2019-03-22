33f0568baade0aa10007ff61b6b3a83f53b92568
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-03-22 10:53:41 +0530

    adding kickstart

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: '',
+    message:''
   };
 
   async componentDidMount(){
@@ -23,12 +25,62 @@ class App extends Component {
 
     this.setState({manager, players, balance});
   }
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value,'ether')
+    });
+
+    this.setState({message: 'You have been entered!'});
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message:'A winner has been picked!'});
+  };
+
   render() {
     return (
 
-      <div className="App">
+      <div className="App container">
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance,'ether')} ether.
+        </p>
+        <hr />
+
+        <form className="form-group" onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter    </label>
+            <input className="form-control"
+              value={this.state.value}
+              onChange={event => this.setState({value: event.target.value})}
+            />
+          </div>
+          <button className="btn btn-primary">Enter</button>
+        </form>
+        <hr />
+        <h4>Ready to pick a winner?</h4>
+        <button className="btn btn-primary" onClick={this.onClick}>Pick a winner!</button>
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
